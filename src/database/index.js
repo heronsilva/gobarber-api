a69dc850afa4c0e59c91704debec4e35919d0045
Heron Silva <heron.code@outlook.com>
2019-10-28 02:14:20 -0300

    Created the files entity

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,10 @@
 import Sequelize from 'sequelize'
 import dbConfig from '../config/database'
 
+import File from '../app/models/File'
 import User from '../app/models/User'
 
-const models = [User]
+const models = [File, User]
 
 class Database {
   constructor() {
@@ -13,7 +14,9 @@ class Database {
   init() {
     this.connection = new Sequelize(dbConfig)
 
-    models.map(model => model.init(this.connection))
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models))
   }
 }
 
