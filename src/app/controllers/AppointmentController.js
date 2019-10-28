de418219922b39083fc6639c4668588ccbf9bb19
Heron Silva <heron.code@outlook.com>
2019-10-28 20:51:13 -0300

    Created the user's appointments list

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -2,9 +2,41 @@ import * as Yup from 'yup'
 import { isBefore, parseISO, startOfHour } from 'date-fns'
 
 import Appointment from '../models/Appointment'
+import File from '../models/File'
 import User from '../models/User'
 
 class AppointmentController {
+  async index(req, res) {
+    const { page = 1 } = req.query
+
+    const appointments = await Appointment.findAll({
+      attributes: ['id', 'date'],
+      order: ['date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      where: {
+        user_id: req.userId,
+        canceled_at: null,
+      },
+      include: [
+        {
+          as: 'provider',
+          model: User,
+          attributes: ['id', 'name'],
+          include: [
+            {
+              attributes: ['id', 'path', 'url'],
+              as: 'avatar',
+              model: File,
+            },
+          ],
+        },
+      ],
+    })
+
+    return res.json(appointments)
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       date: Yup.date().required(),
