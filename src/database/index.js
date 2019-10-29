0c8396b2a2226dd2a9122aa937cae3f3f5279101
Heron Silva <heron.code@outlook.com>
2019-10-29 00:12:55 -0300

    Setup mongoose

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,6 @@
 import Sequelize from 'sequelize'
+import mongoose from 'mongoose'
+
 import dbConfig from '../config/database'
 
 import File from '../app/models/File'
@@ -10,6 +12,7 @@ const models = [File, User, Appointment]
 class Database {
   constructor() {
     this.init()
+    this.mongo()
   }
 
   init() {
@@ -18,6 +21,16 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models))
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+      }
+    )
   }
 }
 
