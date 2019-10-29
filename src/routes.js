806e8f661012ea36ab3d85305d5fa71b7f57a06e
Heron Silva <heron.code@outlook.com>
2019-10-28 21:51:28 -0300

    Created the provider's schedule

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import FileController from './app/controllers/FileController'
 import UserController from './app/controllers/UserController'
 import ProviderController from './app/controllers/ProviderController'
 import AppointmentController from './app/controllers/AppointmentController'
+import ScheduleController from './app/controllers/ScheduleController'
 import SessionController from './app/controllers/SessionController'
 
 const routes = new Router()
@@ -25,6 +26,8 @@ routes.get('/providers', ProviderController.index)
 
 routes.get('/appointments', AppointmentController.index)
 routes.post('/appointments', AppointmentController.store)
+
+routes.get('/schedule', ScheduleController.index)
 
 routes.post('/files', upload.single('file'), FileController.store)
 
