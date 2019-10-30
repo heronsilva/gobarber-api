7414c2465e0625d0f344291e1ef04573ab2e2b87
Heron Silva <heron.code@outlook.com>
2019-10-30 00:38:39 -0300

    Setup nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '2ff5bb1124e008',
+    pass: '215685f239ce01',
+  },
+  default: {
+    from: 'GoBarber Team <noreply@gobarber.com>',
+  },
+}
