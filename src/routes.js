cb5c0ebca9683b83f483e7400fb80715ae645efe
Heron Silva <heron.code@outlook.com>
2019-10-30 10:47:52 -0300

    Listing available schedule for new appointments

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer'
 
 import authMiddleware from './app/middlewares/auth'
 
+import AvailableController from './app/controllers/AvailableController'
 import FileController from './app/controllers/FileController'
 import UserController from './app/controllers/UserController'
 import ProviderController from './app/controllers/ProviderController'
@@ -24,6 +25,7 @@ routes.use(authMiddleware)
 routes.put('/users', UserController.update)
 
 routes.get('/providers', ProviderController.index)
+routes.get('/providers/:id/available', AvailableController.index)
 
 routes.get('/appointments', AppointmentController.index)
 routes.post('/appointments', AppointmentController.store)
